17757329b4d24e0732c2a71d08491a75495492fc
TheosLaptop <pandazflipperpanda706@gmail.com>
2024-03-05 11:59:57 +1300

    idk

--- OpenGL Week 1/GraphicsEngine.cpp
+++ OpenGL Week 1/GraphicsEngine.cpp
@@ -25,8 +25,11 @@ ShaderProgramPtr GraphicsEngine::createShaderProgram(const ShaderProgramDesc& de
 
 void GraphicsEngine::clear(const Vec4& color)
 {
+    //glEnable(GL_DEPTH_TEST);
     glClearColor(color.x, color.y, color.z, color.w);
     glClear(GL_COLOR_BUFFER_BIT);
+    //glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    //glDepthFunc(GL_LESS);
 }
 
 void GraphicsEngine::setFaceCulling(const CullType& type)
--- OpenGL Week 1/Window.cpp
+++ OpenGL Week 1/Window.cpp
@@ -4,12 +4,31 @@
 Window::Window()
 {
     // Set GLFW window hints
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 
+    // Specify whether to create a forward-compatible context
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    //
+    //// Set the number of samples for multisampling
+    //glfwWindowHint(GLFW_SAMPLES, 4);
+    //
+    //glfwWindowHint(GLFW_RED_BITS, 8);
+    //glfwWindowHint(GLFW_GREEN_BITS, 8);
+    //glfwWindowHint(GLFW_BLUE_BITS, 8);
+    //glfwWindowHint(GLFW_ALPHA_BITS, 8);
+    //
+    //glfwWindowHint(GLFW_DOUBLEBUFFER, GL_TRUE);
+    //
+    ////glfwWindowHint(GLFW_DEPTH_BITS, 24);
+    //glfwWindowHint(GLFW_STENCIL_BITS, 8);
+
+    
+
     // Create a GLFW window
-    m_windowPtr = std::shared_ptr<GLFWwindow>(glfwCreateWindow(m_size.width, m_size.height, "TheoCreates | OpenGL 3D Game", nullptr, nullptr));
+    m_windowPtr = std::shared_ptr<GLFWwindow>(glfwCreateWindow(m_size.width, m_size.height, "TheoCreates | OpenGL 3D Game", NULL, NULL));
     if (!m_windowPtr)
     {
         OGL3D_ERROR("GLFW failed to initialize properly. Terminating program.");
